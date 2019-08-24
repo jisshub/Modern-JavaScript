61b3420d1903d2d8ee2802d3b9f3e57cd5a596b3
jisshub <jissmon476@gmial.com>
2019-08-24 12:57:00 +0530

    strarted js and done up to variables

---
+++ apps.js
@@ -0,0 +1,29 @@
+
+// Using the console
+console.log('hello world')
+// boolean 
+console.log('True')
+// arrays
+console.log([12,23,45])
+console.table({a:2, b:3})
+
+// error & warnings in console
+console.error('this is an error message')
+console.warn('this is a warning')
+
+// it shows time taken to print welcome on console
+console.time()
+    console.log('Welcome')
+    console.log('Welcome')
+    console.log('Welcome')
+    console.log('Welcome')
+    console.log('Welcome')
+console.timeEnd()
+
+
+// using variables
+var greeeting = 'hello'
+console.log(greeeting)
+
+
+
---
+++ variables.js
@@ -0,0 +1,87 @@
+
+
+// VARIBLABLES -LET AND CONST
+
+// Rules for initialising variables
+// ---------------------------------------
+// letter, numbers, underscores, $
+
+// cannot start with a num.
+
+// -------------------------------------------------------
+
+// using LET to initialize a variable
+let name = 'Jissmon Jose'
+console.log(name)
+
+// // can reassign variabe name wuith new value
+name = 'Corey Schafer'
+console.log(name)
+
+// below shows err. since variable name already declared
+// let name
+name = 'Donald'
+console.log(name)
+
+let new_name 
+new_name='Donald'
+console.log(new_name)
+
+// --------------------------------------------------
+
+// Using const to declare variables
+const name = 'Ajith'
+console.log(name)
+
+// while declaring a varibale using const, it shud be assigned
+// wth  a value. else error
+// eg:
+const greeting
+
+
+// cannot reassign to same varible that alread assigned.
+const fullname = 'Aju Shaju'
+console.log(fullname)
+// tring to reassgin throws err.
+fullname = 'John Doe'
+
+
+// using const to declare object
+// v cdeclaraed a person object and data goes like key:value pairs
+const person = {
+    name:'jiss',
+    age:32
+}
+
+person.name = 'jissmon'
+person.age = 45
+
+console.log(person)
+
+// bt cant use the same object using const,
+// eg:
+const person =
+{
+    name: 'jiss',
+    age: 34
+}
+
+
+// declaring arrays
+
+const numbers = [2,4,5,6,7]
+console.log(numbers)
+
+// bt cant reassign numbers array again.
+const numbers = [45,2,1]
+
+
+
+
+
+
+
+
+
+
+
