79deba281fb4a8dc60a0203eea038adcea2be9e1
jisshub <jissmon476@gmial.com>
2019-09-01 09:41:53 +0530

    event objects

---
+++ js_dom/dom_part4.js
@@ -0,0 +1,82 @@
+
+
+// REPLACING THE ELEMENT WITH A NEW ELEMENT
+// -------------------------------------------
+
+// first - create an element
+
+const newHeading = document.createElement('h2')
+newHeading.id = 'task-title'
+
+
+// add a texNode
+newHeading.appendChild(document.createTextNode('Task-Title'))
+
+// next - fetch the elemnt to be replaced
+oldHeading = document.querySelector('#task-title')
+
+
+// get the parent node of node to be replaced
+parentNode = document.querySelector('.card-action')
+
+
+// use replaceChild() function and pass both nodes aas args.  use parentnode.replaceChild else error shows.
+parentNode.replaceChild(newHeading, oldHeading)
+
+
+// REMOVING A LIST ITEM
+// --------------------------
+
+const lis = document.querySelectorAll('li')
+// use remove() method of array
+lis[1].remove()
+
+
+
+// CLASSES AND ATTRBIUTES 
+// -------------------------
+
+const firstLi = document.querySelector('li:first-child')
+
+// get the classes
+// ---------------
+
+let val
+
+const link = firstLi.children[0]
+val = link.className
+
+// gives string of  classes
+console.log(val)
+
+// use classlIst property
+// gives a dom tokenlist
+val = link.classList
+console.log(val)
+
+// to acces them individuall
+val = link.classList[1]
+console.log(val)
+
+// // to add class
+link.classList.add('test')
+console.log(link)
+
+// remove class
+link.classList.remove('test')
+console.log(link)
+
+
+// ATTRIBUTES
+// -------------------
+// GET ATTRIBUTE
+
+val = link.getAttribute('href')
+console.log(val)
+
+// set attribute
+link.setAttribute('href', 'https://www.youtube.com')
+
+link.setAttribute('href', 'https://www.udemy.com')
+
+
---
+++ js_dom/dom_part5.js
@@ -0,0 +1,39 @@
+// addEventListener
+// ---------------------------
+// here when v click the element with f=given class,
+// function executes.
+document.querySelector('.clear-tasks').addEventListener('click', function()
+{
+    console.log('Coder')
+})
+// while v clcick the link it doent print it in console. to log it in console.
+// provide a # to the href of the link.
+
+// Method-2- bettermthod. use onClick()
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick)
+
+function onClick()
+{
+    console.log('Hello World')
+}
+
+
+// using event object
+// -------------------
+// here v use an event parameter e and call target and and using it v can change attributes.
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick)
+function onClick(e)
+{
+    
+    e.target.innerText = 'Delete Task'
+    // here v change the text of btn while v click on it.
+}
+
+
+
+
+
+
+
